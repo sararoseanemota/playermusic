d5e5b82c89a95396e098d4142444b8275c6bd825
sararoseanesm@gmail.com <sararoseanemota>
2024-03-29 14:39:28 -0300

    initial commit

---
+++ js/songs.js
@@ -0,0 +1,17 @@
+export default [
+    {
+        src: "./src/RÜFÜS DU SOL -  Innerbloom.mp3",
+        name: 'Innerbloom',
+        artist: 'Rufus Du Sol',
+    },
+    {
+        src: "../src/Bhaskar, 3030 - Manhã de Sol.mp3",
+        name: 'Manhã de Sol',
+        artist: 'Bhaskar',
+    },
+    {
+        src: "../src/Poesia Acústica- Capricorniana.mp3",
+        name: 'Poesia Acústica #3 - Capricorniana',
+        artist: 'Sant _ Tiago Mac _ Lord _ Maria _ Choice',
+    }
+]
